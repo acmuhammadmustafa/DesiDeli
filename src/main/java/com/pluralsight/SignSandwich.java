7fbbae4248e46125d4ca9cfdef3ec7a2bb0cdff2
origin <acmuhammadmustafa@gmail.com>
2025-11-11 11:52:02 -0500

    attempting to create bonus part of capstone, created SignSandwich

---
+++ src/main/java/com/pluralsight/SignSandwich.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public abstract class SignSandwich extends Sandwich {
+
+    public SignSandwich(String bread, int length, boolean toasted){
+        super(bread,length,toasted);
+    }
+
+}
